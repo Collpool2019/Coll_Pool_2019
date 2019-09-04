6485a70f94688eac13aeca8b16d7c54637f5561f
Nishil-Agrawal <49818980+Nishil-Agrawal@users.noreply.github.com>
2019-09-04 13:21:58 +0530

    V0.1.1

--- app/src/main/java/com/example/cpgui/activity_login.java
+++ app/src/main/java/com/example/cpgui/activity_login.java
@@ -3,6 +3,9 @@ package com.example.cpgui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_login extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class activity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void onStartLogin(View view)
+    {
+        EditText Username=(EditText)findViewById(R.id.username);
+        EditText Password=(EditText)findViewById(R.id.password);
+        if((Username.getText().toString().equals("admin"))&&(Password.getText().toString().equals("1234")))
+        {
+            CharSequence prompt = "Welcome " +Username.getText().toString();
+            int length= Toast.LENGTH_SHORT;
+
+            Toast toast=Toast.makeText(this,prompt,length);
+            toast.show();
+        }
+        /*else
+        {
+            CharSequence prompt = "Welcome" +Username.getText().toString();
+            int length= Toast.LENGTH_SHORT;
+
+            Toast toast=Toast.makeText(this,prompt,length);
+            toast.show();
+        }*/
     }
 }
